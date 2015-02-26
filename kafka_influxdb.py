2bc5c6aa649970d92db0c243100437298d39dbd3
Ynguibjoerg <f.punkt.i.punkt@web.de>
2015-02-26 16:54:11 +0100

    First attempt using new class Influxdbdata09

--- kafka_influxdb.py
+++ kafka_influxdb.py
@@ -7,9 +7,24 @@ import argparse
 DB_VERSION_DEFAULT = 0.8
 DB_VERSION_APICHANGE = 0.9
 
+class InfluxDBData09(object):
+	def __init__(self, database, retention_policy):
+		self.database = database
+		self.retentionPolicy = retention_policy
+		self.points = []
+
+	def add_points(self, points): # TODO pointers?
+		self.points.append(points)
+
+	def to_json(self):
+		return json.dumps(self.__dict__)
+
+	def reset(self):
+		self.points = []
+
 class InfluxDBData(object):
 	def __init__(self, name, columns):
-		self.name = name	
+		self.TODOname = name	
 		self.columns = columns
 		self.points = []
 
@@ -18,6 +33,9 @@ class InfluxDBData(object):
 	
 	def to_json(self):
 		return json.dumps(self.__dict__)
+
+	def reset(self):
+		self.points = []
 
 def main(config):
 	# Kafka settings
@@ -31,26 +49,60 @@ def main(config):
 
 	try:
 		db_version = float(config.influxdb_version)
-		transform_to_0_9 = db_version >= DB_VERSION_APICHANGE
+		version_0_9 = db_version >= DB_VERSION_APICHANGE
 	except:
-		transform_to_0_9 = False
+		version_0_9 = False
 
 	# Consume messages
 	consumer = SimpleConsumer(kafka, config.kafka_group, config.kafka_topic)
-	for message in consumer:
+	
+	if version_0_9:
+		stats = InfluxDBData09(config.influxdb_dbname, config.influxdb_retention_policy)
+	else:
 		stats = InfluxDBData(config.influxdb_data_name, config.influxdb_columns)
+	i = 0
+	for message in consumer:
+		i = i + 1
 		val = message.message.value
-		if transform_to_0_9:
-			transform_to_0_9(val)
-		stats.add_point(val)
-		data = json.dumps([stats.__dict__])
-		client.write_points(data)
+		if version_0_9:
+			stats.add_points(transform_to_0_9(val))
+		else:
+			stats.add_point(val)
+		if i == config.buffer_size or config.buffer_size == 0:
+			data = json.dumps([stats.__dict__])
+			print data # TODO remove
+			client.write_points(data)
+			stats.reset()
+			exit() # todo remove
 	kafka.close()
 
-def transform_to_0_9(*point):
-	print point
-	exit()
-	pass #TODO
+def transform_to_0_9(kafka_message): # TODO add error handling
+	#print "Original kafka input:" # TODO remove
+	#print kafka_message # TODO remove
+	results = []
+	for json_obj in json.loads(kafka_message):
+		#print "Tuple element: "  # TODO remove
+		#print json_obj # TODO remove
+		timestamp = json_obj['time'] # todo might need conversion? is a float!
+		tags = {}
+		tags['host'] = json_obj['host']
+		if json_obj['plugin_instance'] != u'':
+			tags['plugin_instance'] = json_obj['plugin_instance']
+		if json_obj['type_instance'] != u'':
+			tags['type_instance'] = json_obj['type_instance']
+		if json_obj['type'] != u'':
+			tags['type'] = json_obj['type']
+		for i in range (0, len(json_obj['values'])):
+			# TODO check that range is defined correctly (borders)
+			new_point = {}
+			new_point["name"] = json_obj['plugin']
+			new_point["timestamp"] = timestamp
+			new_point["tags"] = tags
+			# TODO append i indexed dstype and dsvalue if not empty to tags and check that that really works
+			new_point["fields"] = {"value" : json_obj['values'][i]}
+			results.append(new_point)
+			
+	return results
 
 def parse_args():
 	parser = argparse.ArgumentParser(description='A Kafka consumer for InfluxDB',
@@ -67,6 +119,8 @@ def parse_args():
 	parser.add_argument('--influxdb_data_name', type=str, default='statsd', required=False)
 	parser.add_argument('--influxdb_columns', type=str, default=['counter'], required=False)
 	parser.add_argument('--influxdb_version', type=str, default=[DB_VERSION_DEFAULT], required=False)
+	parser.add_argument('--buffer_size', type=int, default=[1000], required=False)
+	parser.add_argument('--influxdb_retention_policy', type=str, default=['365d'], required=False)
 	return parser.parse_args()
 
 if __name__ == '__main__':
