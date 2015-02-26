fa7a94dc1537312c24e0e8ec916742d13534ff2e
Ynguibjoerg <f.punkt.i.punkt@web.de>
2015-02-26 15:38:50 +0100

    added influxdb option for v0.9 format

--- kafka_influxdb.py
+++ kafka_influxdb.py
@@ -4,6 +4,9 @@ from influxdb import InfluxDBClient
 import json
 import argparse
 
+DB_VERSION_DEFAULT = 0.8
+DB_VERSION_APICHANGE = 0.9
+
 class InfluxDBData(object):
 	def __init__(self, name, columns):
 		self.name = name	
@@ -26,16 +29,28 @@ def main(config):
 				config.influxdb_password,
 				config.influxdb_dbname)
 
+	try:
+		db_version = float(config.influxdb_version)
+		transform_to_0_9 = db_version >= DB_VERSION_APICHANGE
+	except:
+		transform_to_0_9 = False
 
 	# Consume messages
 	consumer = SimpleConsumer(kafka, config.kafka_group, config.kafka_topic)
 	for message in consumer:
 		stats = InfluxDBData(config.influxdb_data_name, config.influxdb_columns)
 		val = message.message.value
+		if transform_to_0_9:
+			transform_to_0_9(val)
 		stats.add_point(val)
 		data = json.dumps([stats.__dict__])
 		client.write_points(data)
 	kafka.close()
+
+def transform_to_0_9(*point):
+	print point
+	exit()
+	pass #TODO
 
 def parse_args():
 	parser = argparse.ArgumentParser(description='A Kafka consumer for InfluxDB',
@@ -51,6 +66,7 @@ def parse_args():
 	parser.add_argument('--influxdb_dbname', type=str, default='kafka', required=False)
 	parser.add_argument('--influxdb_data_name', type=str, default='statsd', required=False)
 	parser.add_argument('--influxdb_columns', type=str, default=['counter'], required=False)
+	parser.add_argument('--influxdb_version', type=str, default=[DB_VERSION_DEFAULT], required=False)
 	return parser.parse_args()
 
 if __name__ == '__main__':
