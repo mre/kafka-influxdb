30f4e0eca2936eb07fff79b2969d0fd0cd307d09
Ynguibjoerg <f.punkt.i.punkt@web.de>
2015-02-27 13:06:21 +0100

    Added statistics. Remaining TODOs: add all tags

--- kafka_influxdb.py
+++ kafka_influxdb.py
@@ -3,10 +3,13 @@ from kafka.consumer import SimpleConsumer
 from influxdb import InfluxDBClient # TODO import dynamically. If version 0.9 we have different requirements!
 import json
 import argparse
+import time
 from collections import defaultdict
 
 DB_VERSION_DEFAULT = 0.8
 DB_VERSION_APICHANGE = 0.9
+
+
 
 #	python kafka_influxdb.py --kafka_topic perfmonspring1 --influxdb_user root --influxdb_password root --influxdb_dbname mydb --influxdb_version 0.9 --kafka_host 10.1.3.234 --influxdb_host springfield02.local --buffer_size 2
 # TODO remove ^
@@ -62,14 +65,20 @@ def main(config):
 	else:
 		stats = InfluxDBData(config.influxdb_data_name, config.influxdb_columns)
 	i = 0
-	j = 0
+	count_flush = 0
+	count_kafka = 0
+	count_datapoints = 0
+	starttime = get_millis()
 	print "Version 9: %s" % version_0_9
 	for message in consumer:
 		i = i + 1
-		#print "Loop %d" %i # TODO remove
 		val = message.message.value
 		if version_0_9:
-			stats.add_points(transform_to_0_9(val))
+			transformed = transform_to_0_9(val)
+			stats.add_points(transformed)
+			if config.statistics:
+				count_datapoints = count_datapoints + len(transformed)
+						
 		else:
 			stats.add_point(val)
 		if i == config.buffer_size or config.buffer_size == 0:
@@ -77,18 +86,28 @@ def main(config):
 				data = stats.points		
 			else:
 				data = [stats.__dict__]
-			#print data # TODO remove
-			#print "Data type: {0}".format(type(data))	# TODO remove
 			client.write_points(data)
-			stats.reset()
+			
+			if config.statistics:
+				count_kafka = count_kafka + i	
+				count_flush = count_flush + 1
+				now = get_millis()
+				time_elapsed_ms = (now - starttime)
+				if config.verbose:
+					print "Time elapsed: %d - %d = %d" % (now, starttime, time_elapsed_ms)
+				kafka_per_ms = (float(count_kafka) / time_elapsed_ms)
+				kafka_per_sec = kafka_per_ms * 1000
+				if config.verbose:
+					print "Count kafka: %d / %d = %d * 1000 = %d" % (count_kafka, time_elapsed_ms , kafka_per_ms, kafka_per_sec)
+				print "Flush %d. Buffer size %d. Parsed %d kafka into %d datapoints. Speed: %d kafka/sec" % (count_flush, config.buffer_size, count_kafka, count_datapoints, kafka_per_sec)
 			i = 0
-			j = j + 1
-			if config.verbose:
-				print "Flush %d w/ buffer size %d" % (j, config.buffer_size)
+			stats.reset()
+			
 	kafka.close()
 
 
-
+def get_millis():
+	return int(round(time.time() * 1000))
 
 def transform_to_0_9(kafka_message): # TODO add error handling
 	results = []
@@ -132,6 +151,7 @@ def parse_args():
 	parser.add_argument('--buffer_size', type=int, default=1000, required=False)
 	parser.add_argument('--influxdb_retention_policy', type=str, default='', required=False)
 	parser.add_argument('--verbose', type=bool, default=False, required=False)
+	parser.add_argument('--statistics', type=bool, default=False, required=False)
 	return parser.parse_args()
 
 if __name__ == '__main__':
