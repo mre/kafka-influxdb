363ab13a05faf26922fb963ad42c2d05e8b1c91f
IngaFeick <inga.feick@trivago.com>
2015-07-03 13:17:13 +0200

    Moved missing method into correct file

--- clients/kafkalistener.py
+++ clients/kafkalistener.py
@@ -38,7 +38,7 @@ class KafkaListener(object):
 			
 			val = message.message.value
 			if self.version_0_9:
-				transformed = transform_to_0_9(val)
+				transformed = self.transform_to_0_9(val)
 				self.stats.add_points(transformed)
 				if self.config.statistics:
 					self.count_datapoints = self.count_datapoints + len(transformed)	
@@ -84,3 +84,37 @@ class KafkaListener(object):
 			
 		except Exception as inst:
 			error_log("Exception caught while flushing: %s" % inst)
+
+	def transform_to_0_9(kafka_message):
+		results = []
+		try:
+			for json_obj in json.loads(kafka_message):
+				timestamp = int(json_obj['time'])
+				tags = {}
+				tags['host'] = json_obj['host']
+				if json_obj['plugin_instance'] != u'':
+					tags['plugin_instance'] = json_obj['plugin_instance']
+				if json_obj['type_instance'] != u'':
+					tags['type_instance'] = json_obj['type_instance']
+				if json_obj['type'] != u'':
+					tags['type'] = json_obj['type']
+				for i in range (0, len(json_obj['values'])):
+					
+					if json_obj['dstypes'][i] != u'':
+						tags['ds_type'] = json_obj['dstypes'][i]
+					else:
+						del tags['ds_type'] #in case this has been set in a previous loop iteration
+					if json_obj['dsnames'][i] != u'':
+						tags['ds_name'] = json_obj['dsnames'][i]
+					else:
+						del tags['ds_name'] #in case this has been set in a previous loop iteration
+					new_point = {"precision":"s"}
+					new_point["name"] = json_obj['plugin']
+					new_point["timestamp"] = timestamp
+					new_point["tags"] = tags
+					
+					new_point["fields"] = {"value" : json_obj['values'][i]}
+					results.append(new_point)
+		except Exception as inst:
+			error_log("Exception caught in json transformation: %s" % inst)
+		return results
--- kafka_influxdb.py
+++ kafka_influxdb.py
@@ -66,39 +66,7 @@ def error_log(msg, die=False):
 def get_millis():
 	return int(round(time.time() * 1000))
 
-def transform_to_0_9(kafka_message):
-	results = []
-	try:
-		for json_obj in json.loads(kafka_message):
-			timestamp = int(json_obj['time'])
-			tags = {}
-			tags['host'] = json_obj['host']
-			if json_obj['plugin_instance'] != u'':
-				tags['plugin_instance'] = json_obj['plugin_instance']
-			if json_obj['type_instance'] != u'':
-				tags['type_instance'] = json_obj['type_instance']
-			if json_obj['type'] != u'':
-				tags['type'] = json_obj['type']
-			for i in range (0, len(json_obj['values'])):
-				
-				if json_obj['dstypes'][i] != u'':
-					tags['ds_type'] = json_obj['dstypes'][i]
-				else:
-					del tags['ds_type'] #in case this has been set in a previous loop iteration
-				if json_obj['dsnames'][i] != u'':
-					tags['ds_name'] = json_obj['dsnames'][i]
-				else:
-					del tags['ds_name'] #in case this has been set in a previous loop iteration
-				new_point = {"precision":"s"}
-				new_point["name"] = json_obj['plugin']
-				new_point["timestamp"] = timestamp
-				new_point["tags"] = tags
-				
-				new_point["fields"] = {"value" : json_obj['values'][i]}
-				results.append(new_point)
-	except Exception as inst:
-		error_log("Exception caught in json transformation: %s" % inst)
-	return results
+
 
 def parse_args():
 	parser = argparse.ArgumentParser(description='A Kafka consumer for InfluxDB',
