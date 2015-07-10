aeff01ba4231df3fcfb4bc97ac847c1442d6c4dd
Matthias Endler <matthias.endler@trivago.com>
2015-07-10 18:10:37 +0200

    Adjust formatting of tests

--- test/encoder_test/test_collectd_grahite_encoder.py
+++ test/encoder_test/test_collectd_grahite_encoder.py
@@ -7,11 +7,11 @@ class TestCollectdGraphiteEncoder(unittest.TestCase):
         self.encoder = collectd_graphite_encoder.Encoder()
 
     def test_encode_simple(self):
-        msg = "myhost.load.load.shortterm 0.05 1436357630"
-        expected = "load_load_shortterm,host=myhost value=0.05 1436357630"
+        msg = 'myhost.load.load.shortterm 0.05 1436357630'
+        expected = 'load_load_shortterm,host=myhost value="0.05" 1436357630'
         self.assertEqual(self.encoder.encode(msg), expected)
 
     def test_encode_with_prefix(self):
-        msg = "mydatacenter.myhost.load.load.shortterm 0.45 1436357630"
-        expected = "load_load_shortterm,datacenter=mydatacenter,host=myhost value=0.45 1436357630"
+        msg = 'mydatacenter.myhost.load.load.shortterm 0.45 1436357630'
+        expected = 'load_load_shortterm,datacenter=mydatacenter,host=myhost value="0.45" 1436357630'
         self.assertEqual(self.encoder.encode(msg, prefix="mydatacenter.", prefix_tag="datacenter"), expected)
