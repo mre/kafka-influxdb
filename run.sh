7de2e07a611f08458ab7c6e4d9e876a3e65a0470
Matthias Endler <matthias.endler@trivago.com>
2015-07-16 16:20:34 +0200

    Make sure kafka is up in docker-compose setup before trying to consume messages.

--- run.sh
+++ run.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
 
+KAFKA_HOST="kafka"
+KAFKA_PORT=9092
 INFLUXDB_HOST="influxdb"
 INFLUXDB_PORT=8086
+
+while true
+do
+  echo "Waiting for Kafka connection at $KAFKA_HOST:$KAFKA_PORT. This may take a while..."
+  timeout 1 bash -c "cat < /dev/null > /dev/tcp/$KAFKA_HOST/$KAFKA_PORT"
+  if [ $? -eq 0 ]; then
+    break
+  fi
+  sleep 2
+done
+
 
 while true
 do
