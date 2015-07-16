420247f2a5108b8fef84bfc4fc22d9e931281d35
Matthias Endler <matthias.endler@trivago.com>
2015-07-16 17:53:39 +0200

    Keep trying to reconnect if Kafka consumer dies unexpectedly

--- kafka_influxdb/reader/kafka_reader.py
+++ kafka_influxdb/reader/kafka_reader.py
@@ -1,19 +1,37 @@
 # -*- coding: utf-8 -*-
 
 import logging
+import time
 from kafka.client import KafkaClient
 from kafka.consumer import SimpleConsumer
 
 class KafkaReader(object):
 
-    def __init__(self, host, port, group, topic):
+    def __init__(self, host, port, group, topic, connection_wait_time=2):
         """ Initialize Kafka reader """
-        connection = "{0}:{1}".format(host, port)
-        logging.info("Connecting to Kafka at %s...", connection)
+        self.host = host
+        self.port = port
+        self.group = group
+        self.topic = topic
+        self.connection_wait_time = connection_wait_time
+
+    def connect(self):
+        connection = "{0}:{1}".format(self.host, self.port)
+        logging.info("Connecting to Kafka at {}...", connection)
         kafka_client = KafkaClient(connection)
-        self.consumer = SimpleConsumer(kafka_client, group, topic)
+        self.consumer = SimpleConsumer(kafka_client,
+                                       self.group,
+                                       self.topic)
 
     def read(self):
         """ Yield messages from Kafka topic """
-        for raw_message in self.consumer:
-            yield raw_message.message.value
+        while True:
+            try:
+                self.connect()
+                for raw_message in self.consumer:
+                    yield raw_message.message.value
+            except Exception:
+                print "oops..."
+                logging.error("Connection to Kafka lost. Trying to reconnect to {}:{}...",
+                                    self.host, self.port)
+                time.sleep(self.connection_wait_time)
