4578ab2467bf077103727ca79b43c4d9c5395a69
Matthias Endler <matthias.endler@trivago.com>
2015-11-06 19:30:44 +0100

    Fix unit test for tags in collectd_graphite_encoder
    Sorting of tags was removed to make the encoder faster.
    As a consequence we need to test for all tag combinations.

--- kafka_influxdb/tests/encoder_test/test_collectd_graphite_encoder.py
+++ kafka_influxdb/tests/encoder_test/test_collectd_graphite_encoder.py
@@ -13,8 +13,20 @@ class TestCollectdGraphiteEncoder(unittest.TestCase):
 
     def test_encode_with_prefix(self):
         msg = b'mydatacenter.myhost.load.load.shortterm 0.45 1436357630'
-        expected = ['load_load_shortterm,datacenter=mydatacenter,host=myhost value=0.45 1436357630']
-        self.assertEqual(self.encoder.encode(msg, prefix="mydatacenter.", prefix_tag="datacenter"), expected)
+
+        # The official documentation states that tags should be sorted for performance reasons.
+        # As of now they will be sorted on the InfluxDB side anyway (which is probably faster).
+        # (See https://influxdb.com/docs/v0.9/write_protocols/line.html#key for more info)
+        # So we don't sort the tags to make the encoder faster.
+        # As a consequence they can appear in any order. Test for all combinations.
+        expected1 = ['load_load_shortterm,datacenter=mydatacenter,host=myhost value=0.45 1436357630']
+        expected2 = ['load_load_shortterm,host=myhost,datacenter=mydatacenter value=0.45 1436357630']
+
+        if self.encoder.encode(msg, prefix="mydatacenter.", prefix_tag="datacenter") == expected1:
+            return
+        if self.encoder.encode(msg, prefix="mydatacenter.", prefix_tag="datacenter") == expected2:
+            return
+        raise self.failureException()
 
     def test_encode_multiple_values(self):
         msg = b'26f2fc918f50.load.load.shortterm 0.05 1436357630\n' \
