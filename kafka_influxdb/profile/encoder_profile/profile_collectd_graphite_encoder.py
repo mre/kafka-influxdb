2b195cfcde927e4097f94819d0f5217d9953b946
Matthias Endler <matthias.endler@trivago.com>
2015-11-06 17:16:51 +0100

    Add profiling scripts for encoder

---
+++ kafka_influxdb/profile/encoder_profile/profile_collectd_graphite_encoder.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+from profilehooks import profile
+from kafka_influxdb.encoder import collectd_graphite_encoder
+
+
+class ProfileCollectdGraphiteEncoder:
+
+    def __init__(self, num_messages=10000):
+        self.encoder = collectd_graphite_encoder.Encoder()
+        self.sample_messages = b"""
+            26f2fc918f50.load.load.shortterm 0.05 1436357630
+            26f2fc918f50.load.load.midterm 0.05 1436357630
+            26f2fc918f50.load.load.longterm 0.05 1436357630
+            26f2fc918f50.cpu-0.cpu-user 30364 1436357630
+            26f2fc918f50.memory.memory-buffered 743657472 1436357630
+            myhost.load.load.shortterm 122.05 1436357630
+            26f2fc918f50.load.load.shortterm 0.45 1436357630
+            26f2fc918f50.load.load.midterm 0.04 1436357630
+            26f2fc918f50.load.load.longterm 11.05 1436357630
+            26f2fc918f50.cpu-0.cpu-user 30363292920 1436357630
+            26f2fc918f50.memory.memory-buffered 743657472 1436357630
+            """
+        self.messages = b'\n'.join(num_messages * [self.sample_messages])
+
+    @profile
+    def profile_messages(self):
+        self.encoder.encode(self.messages)
+
+if __name__ == '__main__':
+    profiler = ProfileCollectdGraphiteEncoder()
+    profiler.profile_messages()
+
---
+++ kafka_influxdb/profile/encoder_profile/profile_collectd_json_encoder.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from profilehooks import profile
+from kafka_influxdb.encoder import collectd_json_encoder
+
+
+class ProfileCollectdJsonEncoder:
+
+    def __init__(self, num_messages=10000):
+        self.encoder = collectd_json_encoder.Encoder()
+        self.sample_messages= b"""
+            [{"values":[0.6],"dstypes":["gauge"],"dsnames":["value"],"time":1444745144.824,"interval":10.000,"host":"xx.example.internal","plugin":"cpu","plugin_instance":"1","type":"percent","type_instance":"system"}]
+            [{"values":[0.7],"dstypes":["gauge"],"dsnames":["value"],"time":1444745144.824,"interval":10.000,"host":"example.com","plugin":"cpu","plugin_instance":"1","type":"percent","type_instance":"user"}]
+            [{"values":[37.7],"dstypes":["gauge"],"dsnames":["value"],"time":1444745144.824,"interval":10.000,"host":"myhost","plugin":"cpu","plugin_instance":"0","type":"percent","type_instance":"nice"}]
+            [{"values":[0],"dstypes":["gauge"],"dsnames":["value"],"time":1444745145.824,"interval":10.000,"host":"myhost","plugin":"cpu","plugin_instance":"0","type":"percent","type_instance":"interrupt"}]
+            [{"values":[1.1],"dstypes":["gauge"],"dsnames":["value"],"time":1444745136.182,"interval":10.000,"host":"myhost","plugin":"memory","plugin_instance":"","type":"percent","type_instance":"slab_recl"}]
+            """
+        self.messages = b'\n'.join(num_messages * [self.sample_messages])
+
+    @profile
+    def profile_messages(self):
+        self.encoder.encode(self.messages)
+
+if __name__ == '__main__':
+    profiler = ProfileCollectdJsonEncoder()
+    profiler.profile_messages()
+
