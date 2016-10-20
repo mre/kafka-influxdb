5025bff2ca9a4f31a371ecbd9255b1fb92b9cc4d
Matthias Endler <matthias.endler@trivago.com>
2016-10-20 23:16:02 +0200

    Return a list of messages in echo encoder and add mypy type hints

--- kafka_influxdb/encoder/echo_encoder.py
+++ kafka_influxdb/encoder/echo_encoder.py
@@ -1,8 +1,15 @@
+try:
+    # Test for mypy support (requires Python 3)
+    from typing import Text
+except:
+    pass
+
 class Encoder(object):
     @staticmethod
     def encode(msg):
+        # type: (bytes) -> List[bytes]
         """
         Don't change the message at all
         :param msg:
         """
-        return msg
+        return [msg]
