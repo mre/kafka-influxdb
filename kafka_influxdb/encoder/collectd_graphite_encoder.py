c8e92cd7662f4f20f96e50ac3d820d6480f56c18
Klaus Bremer <klaus@Klauss-MacBook-Pro.local>
2016-11-19 16:32:21 +0100

    first commit refactoring

--- kafka_influxdb/encoder/collectd_graphite_encoder.py
+++ kafka_influxdb/encoder/collectd_graphite_encoder.py
@@ -1,5 +1,6 @@
 import logging
-from kafka_influxdb.encoder.escape_functions import influxdb_tag_escaper
+#from kafka_influxdb.encoder.escape_functions import influxdb_tag_escaper
+
 
 try:
     # Test for mypy support (requires Python 3)
@@ -13,31 +14,52 @@ class Encoder(object):
     See https://collectd.org/wiki/index.php/Graphite
 
     Sample measurements this encoder can handle:
-    [prefix.]host.plugin.measurement[.postfix] value timestamp
 
     26f2fc918f50.load.load.shortterm 0.05 1436357630
-    26f2fc918f50.load.load.midterm 0.05 1436357630
-    26f2fc918f50.load.load.longterm 0.05 1436357630
-
-    26f2fc918f50.cpu-0.cpu-user 30364 1436357630
-
-    26f2fc918f50.memory.memory-buffered 743657472 1436357630
-
-    The optional prefix and postifx can be set in the collectd plugin:
-    <Plugin write_kafka>
-        Property "metadata.broker.list" "kafka:9092"
-        <Topic "metrics">
-            Format Graphite
-            GraphitePrefix "myprefix"
-            GraphitePostfix "mypostfix"
-        </Topic>
-    </Plugin>
+    webserver.cpu-0.cpu-user 30364 1436357630
+    datacenter.webserver.memory.memory-buffered 743657472 1436357630
     """
 
-    def __init__(self):
+    def __init__(self, templates=None):
+        self.templates = templates or {}
         self.escape_tag = influxdb_tag_escaper()
 
-    def encode(self,
+    def encode(self, messages):
+        """
+        :param messages: Payload from reader as Bytes. Can hold multiple
+                         messages
+        :return: A list of encoded messages
+        """
+        measurements = []
+        for line in messages.decode().split("\n"):
+            try:
+                name, value, timestamp = line.split()
+            except ValueError as e:
+                logging.debug("Error in encoder: %s", e)
+                continue
+			segments = len(name.split('.'))
+			template = self.templates.get(segments)
+			key = self.create_key(name, template)
+			influx_entry = self.create_entry(key, value, timestamp)
+			measurements.append(influx_entry)
+		return measurements
+
+	@staticmethod
+	def create_key(name, template):
+		if not template:
+			return name
+		return name
+
+	@staticmethod
+	def create_entry(key, value, timestamp):
+		return '{} value={} {}'.format(key, value, timestamp)
+
+
+
+
+
+
+    def x_encode(self,
                msg,
                delimiter='.',
                prefix='',
