7c1049fc282ceb09e76c1d7d6a621c2eaa409e09
Klaus Bremer <klaus@Klauss-MacBook-Pro.local>
2016-11-20 14:59:48 +0100

    graphite.Template: documentation cleanup.

--- kafka_influxdb/template/graphite.py
+++ kafka_influxdb/template/graphite.py
@@ -1,26 +1,19 @@
 """
 Maintains a dictionary of valid Graphite message templates.
-
-Templates allow matching parts of a metric name to be used as tag keys in the stored metric.
-They have a similar format to Graphite metric names.
-The values in between the separators are used as the tag keys.
-The location of the tag key that matches the same position as the Graphite metric section
-is used as the value.
-If there is no value, the Graphite portion is skipped.
-
-Example:
-Graphite input: servers.localhost.cpu.loadavg.10
-Template:       host.resource.measurement*
-Output:         measurement=loadavg.10 tags=host=localhost resource=cpu
-
 See: https://github.com/influxdata/influxdb/tree/master/services/graphite
 """
-from collections import defaultdict
-
 
 class Template(object):
     """
-    Create an O(1) lookup dictionary for quick template access (e.g. by an encoder)
+    Create a lookup dictionary for quick template access (e.g. by an encoder)
+
+    Definitions:
+        metric-name: first part if a Graphite message
+        metric-range: number of dots '.' in a metric-name
+
+    Templates matching a given Graphite message must have the same
+    metric-range. metric-names match on shorter templates if a templates
+    ends with a wildcard '*'. More specific templates match first.
     """
     def __init__(self, templates):
         d = {}
@@ -33,6 +26,7 @@ class Template(object):
         try:
             return self.templates[key]
         except KeyError:
+            # TODO: make this faster?
             key -= 1
             while key >= 0:
                 template = self.templates.get(key)
