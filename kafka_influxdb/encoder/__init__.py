ad56ce17f26a7cd4581fa481d091576a1c0300db
Matthias Endler <matthias.endler@trivago.com>
2016-11-22 13:41:35 +0100

    Fix dynamic loading of encoder and template engine

--- kafka_influxdb/encoder/__init__.py
+++ kafka_influxdb/encoder/__init__.py
@@ -6,10 +6,10 @@ def load_encoder(protocol, config):
     Creates an instance of the given encoder.
     An encoder converts a message from one format to another
     """
-    encoder_module = importlib.import_module(protocol)
+    encoder_module = importlib.import_module("kafka_influxdb.encoder." + protocol)
     encoder_class = getattr(encoder_module, "Encoder")
 
-    template_module = importlib.import_module(protocol)
+    template_module = importlib.import_module("kafka_influxdb.template." + protocol)
     template_class = getattr(template_module, "Template")
 
     # Return an instance of the class
