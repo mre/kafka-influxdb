6264322d304d6453ccf1b35e7516fa7b83b2c866
Matthias Endler <matthias.endler@trivago.com>
2016-11-24 16:15:47 +0100

    Add confluent workaround for Kafka <= 0.8.x

--- kafka_influxdb/reader/confluent.py
+++ kafka_influxdb/reader/confluent.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import logging
-from confluent_kafka import Consumer, KafkaError, KafkaException
+from confluent_kafka import Consumer, KafkaError, KafkaException, TopicPartition
 from kafka_influxdb.encoder.errors import EncoderError
 
 
@@ -10,7 +10,14 @@ class Reader(object):
     A high-performance Kafka consumer based on confluent-kafka, which uses librdkafka internally.
     See: https://github.com/confluentinc/confluent-kafka-python
     """
-    def __init__(self, host, port, group, topic, buffer_size=1000):
+
+    # We need to treat legacy Kafka Versions (< 0.9.0) a little different.
+    # First, they don't work without Zookeeper and confluent-kafka has no support for Zookeeper.
+    # Second, they don't support API discovery.
+    # The specific workarounds required are documented below.
+    KAFKA_VERSION_ZOOKEEPER_OPTIONAL = "0.9.0"
+
+    def __init__(self, host, port, group, topic, broker_version=None):
         """
         Initialize Kafka reader
         """
@@ -18,8 +25,7 @@ class Reader(object):
         self.port = str(port)
         self.group = group
         self.topic = [topic]
-        self.buffer_size = buffer_size
-        self.buffer = []
+        self.broker_version = broker_version
 
         # Initialized on read
         self.consumer = None
@@ -31,7 +37,16 @@ class Reader(object):
         """
         logging.debug('Consumer %s: Partitions assigned: %s', consumer, partitions)
 
-    def _connect(self):
+    def _subscribe(self):
+        if self.broker_version >= self.KAFKA_VERSION_ZOOKEEPER_OPTIONAL:
+            self.consumer.subscribe(self.topic, on_assign=self._on_assign)
+        else:
+            # A workaround for missing Zookeeper support in confluent-python.
+            # Automatic partition rebalancing is not working with Kafka Versions < 0.9.0.
+            # Therefore we manually assign the partitions to the consumer for legacy Kafka versions.
+            self.consumer.assign([TopicPartition(self.topic) for partition in range(0, 20)])
+
+    def _setup_connection(self):
         connection = {
             'bootstrap.servers': self.host + ":" + self.port,
             'group.id': self.group,
@@ -40,9 +55,16 @@ class Reader(object):
                 'auto.offset.reset': 'largest'
             }
         }
+        # Add additional flag based on the Kafka version.
+        if self.broker_version < self.KAFKA_VERSION_ZOOKEEPER_OPTIONAL:
+            connection['broker.version.fallback'] = self.broker_version
+
+        return connection
+
+    def _connect(self):
+        connection = self._setup_connection()
         logging.info("Connecting to Kafka at %s...", connection)
         self.consumer = Consumer(**connection)
-        self.consumer.subscribe(self.topic, on_assign=self._on_assign)
 
     def read(self):
         """
