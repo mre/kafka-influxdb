b4be8d75d59436dd0b5f9ee955ad67da0925df6a
Matthias Endler <matthias.endler@trivago.com>
2016-11-25 11:19:32 +0100

    Refactor kafka readers
    Move some generic methods into ReaderAbstract and only overwrite
    reader specific methods in the reader implementations.

--- kafka_influxdb/reader/confluent.py
+++ kafka_influxdb/reader/confluent.py
@@ -1,58 +1,32 @@
-# -*- coding: utf-8 -*-
-
 import logging
 from confluent_kafka import Consumer, KafkaError, KafkaException, TopicPartition
 from kafka_influxdb.encoder.errors import EncoderError
+from kafka_influxdb.reader.reader import ReaderAbstract
 
 
-class Reader(object):
+class Reader(ReaderAbstract):
     """
     A high-performance Kafka consumer based on confluent-kafka, which uses librdkafka internally.
     See: https://github.com/confluentinc/confluent-kafka-python
     """
 
-    # We need to treat legacy Kafka Versions (< 0.9.0) a little different.
-    # First, they don't work without Zookeeper and confluent-kafka has no support for Zookeeper.
-    # Second, they don't support API discovery.
-    # The specific workarounds required are documented below.
-    KAFKA_VERSION_ZOOKEEPER_OPTIONAL = "0.9.0"
-
-    def __init__(self, host, port, group, topic, broker_version=None):
-        """
-        Initialize Kafka reader
-        """
-        self.host = host
-        self.port = str(port)
-        self.group = group
-        self.topic = [topic]
-        self.broker_version = broker_version
-
-        # Initialized on read
-        self.consumer = None
-
-    @staticmethod
-    def _on_assign(consumer, partitions):
-        """
-        Callback called on partition assignment to consumer
-        """
-        logging.debug('Consumer %s: Partitions assigned: %s', consumer, partitions)
-
     def _subscribe(self):
-        if self.broker_version >= self.KAFKA_VERSION_ZOOKEEPER_OPTIONAL:
-            self.consumer.subscribe(self.topic, on_assign=self._on_assign)
-        else:
+        if self.broker_version < self.KAFKA_VERSION_ZOOKEEPER_OPTIONAL:
             # A workaround for missing Zookeeper support in confluent-python.
             # Automatic partition rebalancing is not working with Kafka Versions < 0.9.0.
             # Therefore we manually assign the partitions to the consumer for legacy Kafka versions.
-            self.consumer.assign([TopicPartition(self.topic) for partition in range(0, 20)])
+            self.consumer.assign([TopicPartition(self.topic, p) for p in range(0, 10)])
+        else:
+            self.consumer.subscribe([self.topic])
 
     def _setup_connection(self):
         connection = {
             'bootstrap.servers': self.host + ":" + self.port,
             'group.id': self.group,
-            'session.timeout.ms': 6000,
+            'offset.store.method': 'broker',
             'default.topic.config': {
-                'auto.offset.reset': 'largest'
+                # TODO: Make this configurable
+                'auto.offset.reset': 'largest'  # smallest
             }
         }
         # Add additional flag based on the Kafka version.
@@ -63,45 +37,39 @@ class Reader(object):
 
     def _connect(self):
         connection = self._setup_connection()
-        logging.info("Connecting to Kafka at %s...", connection)
+        logging.info("Connecting to Kafka with the following settings:\n %s...", connection)
         self.consumer = Consumer(**connection)
-
-    def read(self):
-        """
-        Read from Kafka. Reconnect on error.
-        """
-        for msg in self._handle_read():
-            yield msg
+        self._subscribe()
 
     def _handle_read(self):
         """
-        Read from Kafka. Reconnect on error.
+        Read messages from Kafka.
         """
-        try:
-            self._connect()
-            while True:
-                msg = self.consumer.poll(timeout=1.0)
-                if msg is None:
-                    continue
-                if msg.error():
-                    # Error or event
-                    if msg.error().code() == KafkaError._PARTITION_EOF:
-                        # End of partition event
-                        logging.info('%s [%d] reached end at offset %d with key %s\n',
-                                     msg.topic(), msg.partition(), msg.offset(), str(msg.key()))
-                    else:
-                        raise EncoderError(msg.error())
-                else:
-                    # Proper message
-                    logging.debug('%s [%d] at offset %d with key %s:\n',
-                                  msg.topic(), msg.partition(), msg.offset(), str(msg.key()))
-                    # TODO: Is this still needed?
-                    # otherwise the # writer will add extra \n
-                    # self.buffer.append(msg.value().rstrip('\n'))
-                    # TODO: What about commit handling? self.consumer.commit(async=False)
-                    yield msg.value()
-        except KeyboardInterrupt:
-            logging.info('Aborted by user\n')
-        # Close down consumer to commit final offsets.
-        self.consumer.close()
-        yield self.buffer
+        while True:
+            msg = self.consumer.poll(timeout=1.0)
+            logging.debug(msg)
+            if msg is None:
+                continue
+            if msg.error():
+                self._handle_error(msg)
+            else:
+                # Proper message
+                logging.debug('%s [%d] at offset %d with key %s:\n',
+                              msg.topic(), msg.partition(), msg.offset(), str(msg.key()))
+                # TODO: Is this still needed?
+                # otherwise the # writer will add extra \n
+                # self.buffer.append(msg.value().rstrip('\n'))
+                # TODO: What about commit handling? self.consumer.commit(async=False)
+                yield msg.value()
+
+    @staticmethod
+    def _handle_error(msg):
+        if not msg.error():
+            return
+        # Error or event
+        if msg.error().code() == KafkaError._PARTITION_EOF:
+            # End of partition event
+            logging.info('%s [%d] reached end at offset %d with key %s\n',
+                         msg.topic(), msg.partition(), msg.offset(), str(msg.key()))
+        else:
+            raise EncoderError(msg.error())
--- kafka_influxdb/reader/kafka_python.py
+++ kafka_influxdb/reader/kafka_python.py
@@ -1,28 +1,17 @@
 # -*- coding: utf-8 -*-
-
 import logging
 from kafka import KafkaConsumer
 from kafka.common import ConsumerTimeout
 
 from kafka_influxdb.encoder.errors import EncoderError
+from kafka_influxdb.reader.reader import ReaderAbstract
 
 
-class Reader(object):
+class Reader(ReaderAbstract):
     """
     A Kafka consumer based on kafka-python
     See: https://github.com/dpkp/kafka-python
     """
-    def __init__(self, host, port, group, topic):
-        """
-        Initialize Kafka reader
-        """
-        self.host = host
-        self.port = port
-        self.group = group
-        self.topic = topic
-
-        # Initialized on read
-        self.consumer = None
 
     def _connect(self):
         connection = "{0}:{1}".format(self.host, self.port)
@@ -31,22 +20,14 @@ class Reader(object):
                                       group_id=self.group,
                                       bootstrap_servers=[connection])
 
-    def read(self):
-        """
-        Read from Kafka. Reconnect on error.
-        """
-        for msg in self._handle_read():
-            yield msg
-
     def _handle_read(self):
         """
-        Yield messages from Kafka topic
+        Read messages from Kafka.
         """
         try:
-            self._connect()
             for message in self.consumer:
                 yield message.value
         except ConsumerTimeout as timeout:
             logging.error("Kafka error: %s.", timeout)
-            # The actual reconnect handling is done in the worker
-            raise EncoderError(timeout.message)
+            # The actual reconnect handling is done in the caller
+            raise EncoderError(timeout)
