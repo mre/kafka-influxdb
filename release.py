42c846655b62e8b5f7d3ef593ebed3b3e639aa7c
Matthias Endler <matthias.endler@trivago.com>
2017-04-27 13:19:42 +0200

    Refactor release procedure. Bump version

---
+++ release.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, with_statement
+import os
+import sys
+
+try:
+    import pypandoc
+except ImportError:
+    print('Please install pypandoc first, run pip install pypandoc.')
+    sys.exit()
+
+rst = pypandoc.convert('README.md',
+                        to='rst',
+                        extra_args=["--wrap=none",
+                                    "--columns=200"],
+                        format='markdown_github')
+
+with open('README.rst', 'w+') as f:
+    f.write(rst.encode('utf-8'))
+os.system("python setup.py release")
+os.remove('README.rst')
--- setup.py
+++ setup.py
@@ -1,27 +1,17 @@
 from setuptools import setup, find_packages
 import platform
+import os
 
 # Pull version from source without importing
 # since we can't import something we haven't built yet :)
 exec(open('kafka_influxdb/version.py').read())
 
-# Create reStructuredText README file for PyPi
-# http://stackoverflow.com/a/26737672/270334
-try:
-    import pypandoc
 
-    long_description = pypandoc.convert('README.md',
-                                        to='rst',
-                                        extra_args=["--wrap=none", "--columns=200"],
-                                        format='markdown_github')
-    long_description = long_description.replace("\r", "")
-except OSError as e:
-    print("\n\n!!! pandoc not found, long_description is bad, don't upload this to PyPI !!!\n\n")
-    import io
-
-    # pandoc is not installed, fallback to using raw contents
-    with io.open('README.md', encoding="utf-8") as f:
-        long_description = f.read()
+readme = 'README.md'
+if os.path.exists('README.rst'):
+    readme = 'README.rst'
+with open(readme, 'rb') as f:
+    long_description = f.read().decode('utf-8')
 
 requires = [
     "certifi",
